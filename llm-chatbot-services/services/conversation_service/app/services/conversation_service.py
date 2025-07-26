025fe7a68127d42cc57bc7a3ff878b47032b2c0b
Swayam Swaroop Mishra <swayam@Swayams-MacBook-Pro.local>
2025-07-26 05:54:49 +0530

    Added cache entry expiry and code refactor

--- llm-chatbot-services/services/conversation_service/app/services/conversation_service.py
+++ llm-chatbot-services/services/conversation_service/app/services/conversation_service.py
@@ -5,7 +5,9 @@ from app.models.conversation import Conversation
 from app.services.redis import get_redis_client
 import json
 from config import AppConfig
+from shared import get_logger
 
+logger = get_logger(service_name="conversation_service")
 
 class ConversationService:
     """Conversation Service"""
@@ -15,19 +17,16 @@ class ConversationService:
         self.conversation_repo = repo
         self.redis_client = get_redis_client(app_config)
         self.redis_cache_size = app_config.REDIS_CACHE_SIZE
+        self.redis_entry_expiry_time_in_mins = app_config.REDIS_ENTRY_EXPIRY_TIME_IN_MINS
 
     async def create_conversation(self, conversation: ConversationCreate):
         """Create a new conversation"""
         saved_message = await self.conversation_repo.create_conversation(conversation)
         pydantic_obj = ConversationRead.model_validate(saved_message)
-
-        redis_key = self.__get_cache_key(conversation.user_id)
-
-        await self.redis_client.lpush(redis_key, pydantic_obj.model_dump_json())
-        await self.redis_client.ltrim(redis_key, 0, self.redis_cache_size - 1)
+        await self.__add_entry_to_cache(conversation.user_id, pydantic_obj)
 
         return pydantic_obj
-
+    
     async def get_user_conversations(self, user_id: int, limit: int = 20):
         """Get conversations by user"""
         redis_key = self.__get_cache_key(user_id)
@@ -36,13 +35,25 @@ class ConversationService:
         cached = await self.redis_client.lrange(redis_key, 0, limit - 1)
 
         if cached:
+            logger.info(f"Returning cached conversations for user {user_id}")
             return [ConversationRead(**json.loads(msg)) for msg in cached]
 
+        logger.info(f"No cached conversations found for user {user_id}, fetching from database")
         conversations = await self.conversation_repo.get_conversations_by_user(
             user_id, limit=limit
         )
+
+        logger.info(f"Creating new cache for user {user_id}")
         await self.__create_new_cache(user_id, conversations)
         return conversations
+
+    async def __add_entry_to_cache(self, user_id: int, pydantic_obj: ConversationRead):
+        """Add expiry to the cache entry"""
+        logger.info(f"Adding entry to cache for user {user_id}")
+        redis_key = self.__get_cache_key(user_id)
+        await self.redis_client.lpush(redis_key, pydantic_obj.model_dump_json())
+        await self.redis_client.ltrim(redis_key, 0, self.redis_cache_size - 1)
+        await self.redis_client.expire(redis_key, self.redis_entry_expiry_time_in_mins * 60)
 
     async def __create_new_cache(self, user_id: int, conversations: list[Conversation]):
         """Prime the cache with the new conversations"""
--- llm-chatbot-services/services/conversation_service/config.py
+++ llm-chatbot-services/services/conversation_service/config.py
@@ -21,6 +21,7 @@ class AppConfig:
 
     REDIS_URL: str | None = None
     REDIS_CACHE_SIZE: int | None = None
+    REDIS_ENTRY_EXPIRY_TIME_IN_MINS: int | None = None
 
     def __init__(self):
         self.__set_config()
@@ -30,6 +31,7 @@ class AppConfig:
         """Set the configuration values"""
         self.REDIS_URL = os.getenv("REDIS_URL")
         self.REDIS_CACHE_SIZE = self.__get_redis_cache_size()
+        self.REDIS_ENTRY_EXPIRY_TIME_IN_MINS = self.__get_redis_entry_expiry_time_in_mins()
 
     def __validate_config(self):
         """Validate the configuration values"""
@@ -42,8 +44,18 @@ class AppConfig:
             raise ConfigError(
                 "REDIS_CACHE_SIZE is required but not set in environment variables."
             )
+        
+        if self.REDIS_ENTRY_EXPIRY_TIME_IN_MINS is None:
+            raise ConfigError(
+                "REDIS_ENTRY_EXPIRY_TIME_IN_MINS is required but not set in environment variables."
+            )
 
     def __get_redis_cache_size(self) -> int | None:
         """Get the Redis cache size"""
         value = os.getenv("REDIS_CACHE_SIZE")
+        return int(value) if value else None
+
+    def __get_redis_entry_expiry_time_in_mins(self) -> int | None:
+        """Get the Redis entry expiry time in minutes"""
+        value = os.getenv("REDIS_ENTRY_EXPIRY_TIME_IN_MINS")
         return int(value) if value else None
