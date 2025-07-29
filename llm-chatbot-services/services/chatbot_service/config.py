4def75cdbc1442d8b3d6c39ac0d76776a9e9bfaf
Swayam Swaroop Mishra <swayam@Swayams-MacBook-Pro.local>
2025-07-30 02:46:58 +0530

    Added gemini url in docker-compose and chatbot config

--- llm-chatbot-services/services/chatbot_service/config.py
+++ llm-chatbot-services/services/chatbot_service/config.py
@@ -20,6 +20,8 @@ class AppConfig:
     """Configuration class for the chat service"""
 
     CONVERSATION_SERVICE_URL: str | None = None
+    GEMINI_API_KEY: str | None = None
+    GEMINI_URL: str | None = None
 
     def __init__(self):
         self.__set_config()
@@ -28,10 +30,22 @@ class AppConfig:
     def __set_config(self):
         """Set the configuration values"""
         self.CONVERSATION_SERVICE_URL = os.getenv("CONVERSATION_SERVICE_URL")
+        self.GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+        self.GEMINI_URL = os.getenv("GEMINI_URL")
 
     def __validate_config(self):
         """Validate the configuration values"""
         if self.CONVERSATION_SERVICE_URL is None:
             raise ConfigError(
                 "CONVERSATION_SERVICE_URL is required but not set in environment variables/ docker-compose."
+            )
+        
+        if self.GEMINI_API_KEY is None:
+            raise ConfigError(
+                "GEMINI_API_KEY is required but not set in environment variables/ docker-compose."
+            )
+        
+        if self.GEMINI_URL is None:
+            raise ConfigError(
+                "GEMINI_URL is required but not set in environment variables/ docker-compose."
             )
