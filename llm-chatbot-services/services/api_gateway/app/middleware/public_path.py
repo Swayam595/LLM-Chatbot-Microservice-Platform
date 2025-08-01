d0d863f12f979f0a28829e276fdaedc848a3b71e
Swayam Swaroop Mishra <swayam@Swayams-MacBook-Pro.local>
2025-08-02 01:47:29 +0530

    Added refresh token path in public-path

--- llm-chatbot-services/services/api_gateway/app/middleware/public_path.py
+++ llm-chatbot-services/services/api_gateway/app/middleware/public_path.py
@@ -1,6 +1,7 @@
 public_paths = {
     ("GET", "/auth/me"),
     ("POST", "/auth/login"),
+    ("POST", "/auth/refresh"),
     ("POST", "/auth/register"),
     ("POST", "/auth/forgot-password"),
     ("POST", "/auth/reset-password"),
